1eb24e8741ad6c6ae9afea61a08ec5f5623b02a3
CHizzyEzu <155769930+CHizzyEzu@users.noreply.github.com>
2024-04-12 13:09:32 -0400

    Included a sum, min, max, and average of all the numbers within the program and added additional commentary

--- Lab_5_Project.cpp
+++ Lab_5_Project.cpp
@@ -1,13 +1,14 @@
 // File program that allows you to name your file lists 10 random number 0 - 100
 // Does this 10 times per line for a total of 50 numbers. Formats the numbers neatly as well
-// Lastly file name and information is outputted to console
+// The file name and information is outputted to console
+// Lastly the min, max, average, and sum of all the numbers are outputted at the end of the program
 
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
 #include <string>
-#include <fstream>
+#include <fstream>  
 using namespace std;
 
 int main()
@@ -26,11 +27,13 @@ int main()
         return 1;
     }
 
+    outputFile << yourname << endl;
+
     // RNG parameters
     unsigned seed = time(0);
     srand(seed);
 
-    const int MIN_VALUE = 1;
+    const int MIN_VALUE = 0;
     const int MAX_VALUE = 100;
 
     // RNG loop
@@ -42,33 +45,53 @@ int main()
 
         if ((c + 1) % 10 == 0) // Newline after 10 numbers
             outputFile << endl;
-
-        else
-            outputFile << " "; // spaces added between lines
     }
 
     outputFile.close(); // File closes
-
-    // Terminal output written to file
-    cout << "File name is: " << yourname << ".txt" << endl;
-    cout << "The information in this file is as follows: \n\n";
 
     ifstream inputFile;
     inputFile.open(yourname + ".txt"); // File opens and writes data to yourname.txt
 
     if(!inputFile.is_open())
     {
-        cout << "Error! Unable to open the file." << endl; // File failed to be read
+        cout << "Error! Unable to open the file for reading." << endl; // File failed to be read
         return 1;
     }
 
-    char ch;
-    while (inputFile.get(ch))
+    string nameFromFile;
+    getline(inputFile, nameFromFile);
+    cout << "Name from file: " << nameFromFile << endl;
+
+    // Read and display the numbers , calculate sum, min, max, and average
+    int sum = 0;
+    int min = 0;
+    int max = 0;
+    int count = 0;
+
+    int number; //
+
+    // Loops through numbers in two's, counts the numbers and adds them together
+    while(inputFile >> number)
     {
-        cout << ch; //Output file content to console
+        cout << setw(3) << number << " ";
+        sum += number;
+        if(number < min) min = number;
+        if(number > max) max = number;
+        count++;
+        if(count % 2 == 0) cout << endl; // Start a new line every 2 numbers
+
     }
 
     inputFile.close(); //File closes
+
+    // Holds a decimal value for the average
+    double average = static_cast<double>(sum) / count;
+
+    // Display statistics
+    cout << "\n\nTotal: " << sum << endl;
+    cout << "Minimum: " << min << endl;
+    cout << "Maximum: " << max << endl;
+    cout << "Average: " << fixed << setprecision(2) << average << endl;
 
     return 0; // End of program
 
